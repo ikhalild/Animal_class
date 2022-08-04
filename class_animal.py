de03fc0b01f25f9d4e0406f3e4d42d910b9a7481
Ibrahima Diallo <idiallo@quantifyafrica.com>
2022-08-04 13:25:09 +0000

    creation of a function : travel mode

---
+++ class_animal.py
@@ -0,0 +1,32 @@
+list_animaux = ["guepard","lapin","poisson","pigeon", "autres"]
+
+class Animal:
+    type = str
+    deplacement = str
+    habitat= str
+
+    def __init__(self, type, deplacement, habitat):
+        self.type = type
+        self.deplacement = deplacement
+        self.habitat = habitat
+    
+    def mode_deplacement(self):
+        if  self.type in range ["guepard","lapin"]:
+             self.deplacement = "marche"
+        elif self.type == "poisson":
+             self.deplacement = "nange"  
+        elif self.type == "pigeon":
+             self.deplacement = "vole"
+        elif self.type == "autres":
+            self.deplacement = "inconnu"
+        
+            print(f"Un {self.type} {self.deplacement}.")
+
+   
+
+
+        
+        
+
+            
+
